14581c00dec2c869315e3ea1454228897d48c416
Gustav Sundström <sundstrom-gustav@hotmail.com>
2017-05-02 13:46:47 +0200

    typo

--- app/includes/navigation.php
+++ app/includes/navigation.php
@@ -1,7 +1,7 @@
 <nav>
     <ul>
-        <li><a href="#"> Register</a></li>
-        <li><a href="#"> Login</a></li>
-        <li><a href="#"> View all posts</a></li>
+        <li><a href="#">Sign up</a></li>
+        <li><a href="#">Login</a></li>
+        <li><a href="#">View all posts</a></li>
     </ul>
 </nav>
