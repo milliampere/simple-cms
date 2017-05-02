650ea820440b5e67aaa176fdc020a3a29f79d9fc
Gustav Sundström <sundstrom-gustav@hotmail.com>
2017-05-02 18:35:41 +0200

    create posts

--- app/classes/Post.php
+++ app/classes/Post.php
@@ -1,10 +1,38 @@
 <?php
 
-class Post 
-{
-    public function createPost($pdo){
+// class Post 
+// {
+    // public function createPost($pdo){
+        $options = [ 
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false
+        ];
+
+        $pdo = new PDO(
+        "mysql:host=localhost;dbname=simple-cms;charset=utf8",
+        "root",
+        "root", $options);
+
+        If(isset($_POST['submit'])){
+            try {
+           
+            $stmt = $pdo->prepare("INSERT INTO posts (title, content)
+            VALUES (:postTitle, :postContent)");
+            $stmt->bindParam(':postTitle', $postTitle);
+            $stmt->bindParam(':postContent', $postContent);
+
+            $postTitle = $_POST['postTitle'];
+            $postContent = $_POST['postContent'];
+           
+            $stmt->execute();
+            echo "post skapad";
+        }catch(PDOException $e){
+        echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+        }
         
-    }
+    // }
 
 
-}
