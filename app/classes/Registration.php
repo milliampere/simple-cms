494e79067af6ca7c5f1223d05698761ded95f3d3
petersandqvist <petersandqvist@Peters-MacBook.lan>
2017-05-04 21:28:40 +0200

    Register in the making

--- app/classes/Registration.php
+++ app/classes/Registration.php
@@ -1,8 +1,50 @@
 <?php
 
-class Registration 
-{
+session_start();
 
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false
+];
 
+$pdo = new PDO(
+    "mysql:host=localhost;dbname=simple-cms;charset=utf8",
+    "root",
+    "root", $options);
 
-}
+    ?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Register</title>
+    </head>
+    <body>
+        <div class="header">
+            <hi>Register</h1>
+        </div>
+
+        <form>
+            <table>
+            <tr>
+                <td>Username:</td>
+                <td><input type="text" name="username" class="textInput"></td>
+                </tr> 
+                 <tr>
+                <td>Email:</td>
+                <td><input type="email" name="email" class="textInput"></td>
+                </tr> 
+                 <tr>
+                <td>Password:</td>
+                <td><input type="password" name="password" class="textInput"></td>
+                </tr> 
+                 <tr>
+                <td>Username:</td>
+                <td><input type="text" name="username" class="textInput"></td>
+                </tr>    
+            </table>
+        </form>
+    </body>
+    </html>
+ 
