1ab7998154ba454de650d68b37ce9f1f308e4b53
petersandqvist <petersandqvist@Peters-MacBook.lan>
2017-05-04 21:48:51 +0200

    register_btn

--- app/classes/Registration.php
+++ app/classes/Registration.php
@@ -13,7 +13,19 @@ $pdo = new PDO(
     "root",
     "root", $options);
 
+
+if(isset($_POST['register_btn'])) {
+    $username = mysql_real_escape_string($_POST['username']);
+    $email = mysql_real_escape_string($_POST['email']);
+    $password = mysql_real_escape_string($_POST['password']);
+    $password2 = mysql_real_escape_string($_POST['password2']);
+}
+
     ?>
+
+
+
+
 
     <!DOCTYPE html>
     <html lang="en">
@@ -25,7 +37,7 @@ $pdo = new PDO(
             <hi>Register</h1>
         </div>
 
-        <form>
+        <form method="post" action="Registration.php">
             <table>
             <tr>
                 <td>Username:</td>
@@ -40,9 +52,12 @@ $pdo = new PDO(
                 <td><input type="password" name="password" class="textInput"></td>
                 </tr> 
                  <tr>
-                <td>Username:</td>
-                <td><input type="text" name="username" class="textInput"></td>
-                </tr>    
+                <td>Password again:</td>
+                <td><input type="password" name="password2" class="textInput"></td>
+                </tr>
+                <td></td>
+                <td><input type="submit" name="register_btn" value="Register"></td>
+                </tr>     
             </table>
         </form>
     </body>
