75ebe978a82851614b47d81cc59c4742e62a4e4f
milliampere <millla@hotmail.com>
2017-05-07 10:01:45 +0200

    Structure the code

--- app/logincheck.php
+++ app/logincheck.php
@@ -2,50 +2,44 @@
 session_start();
 include 'database.php';
 
-
-
+// Input from the user
 $email = $_POST['email'];
 $password = $_POST['password'];
 
+// Get user from database
 $stmt = $pdo->prepare("SELECT * FROM users WHERE email='$email'");
 $stmt->execute();
 $data = $stmt->fetch();
 
+// If the email doesn't exist
 if(!$row = $data){
 	//echo "Fel email";
 }
 
-// Function
+// Check if the user is admin
 if($data['isAdmin'] == 1){
 	$isAdmin = true;
 } else {
 	$isAdmin = false;
 }
 
-$hash = $data['password'];
-// Function
-if(password_verify($password, $hash)){
-	$_SESSION['loggedIn'] = true;
-	$_SESSION['email'] = $email;
-	$_SESSION['name'] = $data['name'];
-	$_SESSION['isAdmin'] = $isAdmin;
+// Verify password, set session variables
+function verify($password){
+	$hash = $data['password'];
+	if(password_verify($password, $hash)){
+		$_SESSION['loggedIn'] = true;
+		$_SESSION['email'] = $email;
+		$_SESSION['name'] = $data['name'];
+		$_SESSION['id'] = $data['id'];
+		$_SESSION['isAdmin'] = $isAdmin;
+	}
+	else {
+		//echo "Wrong password";
+	}
 }
-else {
-	//echo "Wrong password";
-}
+
+verify($password);
 
 header("Location: userpage.php");
-
-// Login, verify password 
-/*function verify($pass){
-	if(password_verify($pass, $hash)){
-		$_SESSION['loggedIn'] = true;
-		$_SESSION['username'] = "Camilla";
-	} 
-	else {
-		echo "Liar";
-	}
-}*/
-
 
 ?>
