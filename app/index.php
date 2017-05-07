f2d67b35ea85850b621770160653f403f0d00bb3
milliampere <millla@hotmail.com>
2017-05-07 10:01:05 +0200

    Empty index

--- app/index.php
+++ app/index.php
@@ -1,10 +1,8 @@
 <?php 
 include 'includes/header.php';
 
-include 'database.php';
-include 'classes/User.php';
-		$user = new User;
-		var_dump($user->getName($pdo));
+
+
 
 
 include 'includes/footer.php';
