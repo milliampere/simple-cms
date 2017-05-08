484f2f298250090c8eea4a1650d28b706b0b6a40
milliampere <millla@hotmail.com>
2017-05-08 18:29:35 +0200

    Sign up

---
+++ app/classes/Signup.php
@@ -0,0 +1,39 @@
+<?php
+
+class Signup 
+{
+    private $pdo;
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    // Create user in database
+    public function createuser($name, $email, $password){
+
+      // Check if email exists
+      $stmt = $this->pdo->prepare("SELECT * FROM users WHERE email='$email'");
+      $stmt->execute();
+      $email_exists = $stmt->fetch();
+
+      if ($email_exists == true) {
+        echo "Sorry, the email is already taken.";
+        // Here we have to add the error message to the "register page" not as an echo.
+      } 
+      else {
+        // Crypt password 
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+
+        // Insert into databse
+        $stmt = $this->pdo->prepare("INSERT INTO users (name, email, password)
+        VALUES (:name, :email, :hash)");
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':hash', $hash);
+        $stmt->execute();
+        
+      }
+
+    }
+}
---
+++ app/createuser.php
@@ -0,0 +1,13 @@
+<?php 
+include 'classes/Signup.php';
+include 'classes/Database.php';
+$pdo = Database::connection();
+$signup = new Signup($pdo);
+
+// Input from the user
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$signup->createuser($name, $email, $password);
+
