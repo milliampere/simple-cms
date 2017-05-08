c349f0c1247800c28ec2864a674fe1251645898b
petersandqvist <petersandqvist@Peters-MacBook.lan>
2017-05-08 15:09:10 +0200

    Confirm password placeholder

--- app/register.php
+++ app/register.php
@@ -25,6 +25,12 @@
                 <input type="password" class="form-control" name="password" placeholder="Enter password">
                 </div>
             </div>
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="password">Confirm Password:</label>
+                <div class="col-sm-10"> 
+                <input type="password" class="form-control" name="password2" placeholder="Enter password">
+                </div>
+            </div>
             <div class="form-group"> 
                 <div class="col-sm-offset-2 col-sm-10">
                 <button type="submit" class="btn btn-default" name="signup">Sign up!</button>
@@ -39,3 +45,4 @@
 <?php 
   include 'includes/footer.php';
 ?>
+
