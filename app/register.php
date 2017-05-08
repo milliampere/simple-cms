daf2cabed9789bacf66d0bdfa78da2a084841468
petersandqvist <petersandqvist@Peters-MacBook.lan>
2017-05-08 20:51:48 +0200

    small change

--- app/register.php
+++ app/register.php
@@ -26,7 +26,7 @@
                 </div>
             </div>
             <div class="form-group">
-                <label class="control-label col-sm-2" for="password">Confirm Password:</label>
+                <label class="control-label col-sm-4" for="password">Confirm Password:</label>
                 <div class="col-sm-10"> 
                 <input type="password" class="form-control" name="password2" placeholder="Enter password">
                 </div>
