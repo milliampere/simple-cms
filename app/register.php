81824df023929fc8de25ec03c78886df53059545
milliampere <millla@hotmail.com>
2017-05-08 14:07:45 +0200

    Add Bootstrap style

--- app/register.php
+++ app/register.php
@@ -1,35 +1,41 @@
-<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <title>Register</title>
-    </head>
-    <body>
-        <div class="header">
-            <hi>Register</h1>
-        </div>
+<?php 
+  include 'includes/header.php';  
+?>
 
-        <form method="post" action="Registration.php">
-            <table>
-            <tr>
-                <td>Username:</td>
-                <td><input type="text" name="name" class="textInput"></td>
-                </tr> 
-                 <tr>
-                <td>Email:</td>
-                <td><input type="email" name="email" class="textInput"></td>
-                </tr> 
-                 <tr>
-                <td>Password:</td>
-                <td><input type="password" name="password" class="textInput"></td>
-                </tr> 
-                 <tr>
-                <td>Confirm Password:</td>
-                <td><input type="password" name="password2" class="textInput"></td>
-                </tr>
-                <td></td>
-                <td><input type="submit" name="register_button" value="Register"></td>
-                </tr>     
-            </table>
-        </form>
-    </body>
-    </html>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-6">
+            <h3>Sign up</h3>
+            <form class="form-horizontal" action="createuser.php" method="POST">
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="email">Name:</label>
+                <div class="col-sm-10">
+                <input type="text" class="form-control" name="name" placeholder="Enter name">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="email">Email:</label>
+                <div class="col-sm-10">
+                <input type="email" class="form-control" name="email" placeholder="Enter email">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="password">Password:</label>
+                <div class="col-sm-10"> 
+                <input type="password" class="form-control" name="password" placeholder="Enter password">
+                </div>
+            </div>
+            <div class="form-group"> 
+                <div class="col-sm-offset-2 col-sm-10">
+                <button type="submit" class="btn btn-default" name="signup">Sign up!</button>
+                </div>
+            </div>
+            </form>
+        </div>
+        <div class="col-4"></div>
+    </div>
+</div>
+
+<?php 
+  include 'includes/footer.php';
+?>
