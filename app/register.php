a63b1e5aae0484f161a9f073ba159a0d93f66c28
petersandqvist <petersandqvist@Peters-MacBook.lan>
2017-05-10 10:29:25 +0200

    small changes to validation

--- app/register.php
+++ app/register.php
@@ -16,19 +16,19 @@
             <div class="form-group">
                 <label class="control-label col-sm-2" for="email">Email:</label>
                 <div class="col-sm-10">
-                <input type="email" required class="form-control" name="email" placeholder="Enter email">
+                <input type="email" class="form-control" name="email" required pattern="[^@]+@[^@]+\.[a-zA-Z]{2,6}" placeholder="Enter email">
                 </div>
             </div>
             <div class="form-group">
                 <label class="control-label col-sm-2" for="password">Password:</label>
                 <div class="col-sm-10"> 
-                <input type="password" required class="form-control" name="password" placeholder="Enter password">
+                <input type="password" class="form-control" name="password" required placeholder="Enter password">
                 </div>
             </div>
             <div class="form-group">
                 <label class="control-label col-sm-4" for="password">Confirm Password:</label>
                 <div class="col-sm-10"> 
-                <input type="password" required class="form-control" name="password2" placeholder="Enter password">
+                <input type="password" class="form-control" name="password2" required placeholder="Enter password">
                 </div>
             </div>
             <div class="form-group"> 
