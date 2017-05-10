5a3dbd1f246a37487fc927cd2c11a52971e2db4a
Gustav Sundström <sundstrom-gustav@hotmail.com>
2017-05-10 13:46:36 +0200

    db connection

--- app/classes/Likes.php
+++ app/classes/Likes.php
@@ -1,3 +1,20 @@
 <?php
-    
+include 'classes/User.php';
+include 'classes/Database.php';
+
+
+$pdo = Database::connection();
+
+        try {
+        $statement = $pdo->prepare("UPDATE posts SET likes = likes + 1WHERE id = '".$userid."'");
+        $statement->execute();
+        $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+        
+        }
+        
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
  ?>
