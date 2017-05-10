20f9cf4a57b451d27bedef8d1a49a99b3668c534
petersandqvist <petersandqvist@Peters-MacBook.lan>
2017-05-10 10:34:30 +0200

    password matching fixed

--- app/register.php
+++ app/register.php
@@ -22,13 +22,13 @@
             <div class="form-group">
                 <label class="control-label col-sm-2" for="password">Password:</label>
                 <div class="col-sm-10"> 
-                <input type="password" class="form-control" name="password" required placeholder="Enter password">
+                <input type="password" class="form-control" id="password" name="password" required placeholder="Enter password">
                 </div>
             </div>
             <div class="form-group">
                 <label class="control-label col-sm-4" for="password">Confirm Password:</label>
                 <div class="col-sm-10"> 
-                <input type="password" class="form-control" name="password2" required placeholder="Enter password">
+                <input type="password" class="form-control" id="password2" name="password2" required placeholder="Enter password">
                 </div>
             </div>
             <div class="form-group"> 
