370e30d21055a8eb0feba447a7f0883f14557d47
milliampere <millla@hotmail.com>
2017-05-23 10:14:09 +0200

    Load more nav

--- app/includes/navbar.php
+++ app/includes/navbar.php
@@ -7,7 +7,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="viewposts.php">View all posts <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="_viewposts.php">View all posts <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="register.php">Sign up</a>
