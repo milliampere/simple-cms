f156087726ab265996e2729f6ab89e610251ae8e
milliampere <millla@hotmail.com>
2017-05-23 19:59:00 +0200

    Minor changes

--- app/index.php
+++ app/index.php
@@ -1,33 +1,11 @@
 <?php
     session_start();
-    //include 'classes/Verify.php';
-?>
-
-<?php 
     include 'includes/header.php';
 ?>
 
-<style>
-img {
-	width: 100%;
-	height: 100%;
-    margin-bottom: -10em;
-}
-</style>
+<img src="images/code.jpg" alt="background">
 
-<img src="../src/img/code.jpg" alt="background">
 
-    
-
-    <!--<div class="container">
-        <div class="row">
-            <?php
-                //Verify::isAdmin();            
-            ?>
-            
-        </div>
-    </div>-->
-
-<!--<?php
+<?php
     include 'includes/footer.php';
-?>-->
+?>
